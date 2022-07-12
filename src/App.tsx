604bafd098559b04bc73276db4baa9339a3865ab
bELMUT695 <antunezgonzalesv@gmail.com>
2022-07-12 00:04:19 -0500

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -247,195 +247,197 @@ export default function App() {
   };
 
   return (
-    <div className="quiz-container Ini5MateForma3">
-      <div className="main__5forma__3">
-        <div className="row">
-          <div className="col-12">
-            <div className="container__images relative full-width full-height">
-              <div className="image__fondo full-width full-height">
-                <img
-                  src={fondo}
-                  className="responsive-image_cantidad_044"
-                  alt=""
-                />
-              </div>
+    <div className="exercise-bg exercise-inicial-5">
+      <div className="quiz-container Ini5MateForma3">
+        <div className="main__5forma__3">
+          <div className="row">
+            <div className="col-12">
+              <div className="container__images relative full-width full-height">
+                <div className="image__fondo full-width full-height">
+                  <img
+                    src={fondo}
+                    className="responsive-image_cantidad_044"
+                    alt=""
+                  />
+                </div>
 
-              <div className="barra__right absolute">
-                <div className="relative full-width full-height">
-                  <div className="drag__image__container absolute">
-                    <ContainerDnD
-                      id="opcion1"
-                      className="drag__image full-height full-width relative"
-                    >
-                      <BoxDnD
-                        key="1"
-                        id="drag1"
-                        draggable="true"
-                        dragStart={dragStart}
-                        dragEnd={dragEnd}
-                        dragOverOption={dragOverOption}
-                        img={chupetin1}
+                <div className="barra__right absolute">
+                  <div className="relative full-width full-height">
+                    <div className="drag__image__container absolute">
+                      <ContainerDnD
+                        id="opcion1"
+                        className="drag__image full-height full-width relative"
+                      >
+                        <BoxDnD
+                          key="1"
+                          id="drag1"
+                          draggable="true"
+                          dragStart={dragStart}
+                          dragEnd={dragEnd}
+                          dragOverOption={dragOverOption}
+                          img={chupetin1}
+                        />
+                        <BoxDnD
+                          key="2"
+                          id="drag2"
+                          draggable="true"
+                          dragStart={dragStart}
+                          dragEnd={dragEnd}
+                          dragOverOption={dragOverOption}
+                          img={chupetin2}
+                        />
+                        <BoxDnD
+                          key="3"
+                          id="drag3"
+                          draggable="true"
+                          dragStart={dragStart}
+                          dragEnd={dragEnd}
+                          dragOverOption={dragOverOption}
+                          img={chupetin3}
+                        />
+                        <BoxDnD
+                          key="4"
+                          id="drag4"
+                          draggable="true"
+                          dragStart={dragStart}
+                          dragEnd={dragEnd}
+                          dragOverOption={dragOverOption}
+                          img={chupetin4}
+                        />
+                        <BoxDnD
+                          key="5"
+                          id="drag5"
+                          draggable="true"
+                          dragStart={dragStart}
+                          dragEnd={dragEnd}
+                          dragOverOption={dragOverOption}
+                          img={chupetin5}
+                        />
+                      </ContainerDnD>
+                    </div>
+                    <div className="container__barra full-width full-height">
+                      <img
+                        src={barra}
+                        alt=""
+                        className="full-width full-height"
                       />
-                      <BoxDnD
-                        key="2"
-                        id="drag2"
-                        draggable="true"
-                        dragStart={dragStart}
-                        dragEnd={dragEnd}
-                        dragOverOption={dragOverOption}
-                        img={chupetin2}
-                      />
-                      <BoxDnD
-                        key="3"
-                        id="drag3"
-                        draggable="true"
-                        dragStart={dragStart}
-                        dragEnd={dragEnd}
-                        dragOverOption={dragOverOption}
-                        img={chupetin3}
-                      />
-                      <BoxDnD
-                        key="4"
-                        id="drag4"
-                        draggable="true"
-                        dragStart={dragStart}
-                        dragEnd={dragEnd}
-                        dragOverOption={dragOverOption}
-                        img={chupetin4}
-                      />
-                      <BoxDnD
-                        key="5"
-                        id="drag5"
-                        draggable="true"
-                        dragStart={dragStart}
-                        dragEnd={dragEnd}
-                        dragOverOption={dragOverOption}
-                        img={chupetin5}
-                      />
-                    </ContainerDnD>
-                  </div>
-                  <div className="container__barra full-width full-height">
-                    <img
-                      src={barra}
-                      alt=""
-                      className="full-width full-height"
-                    />
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className="container__drop absolute" id="item1">
-                <ContainerDnD
-                  key="drop1"
-                  id="drop1"
-                  className="image__drop"
-                  drop={drop}
-                  dragOver={dragOver}
-                ></ContainerDnD>
-              </div>
-              <div className="container__drop absolute" id="item2">
-                <ContainerDnD
-                  key="drop2"
-                  id="drop2"
-                  className="image__drop"
-                  drop={drop}
-                  dragOver={dragOver}
-                ></ContainerDnD>
-              </div>
-              <div className="container__drop absolute" id="item3">
-                <ContainerDnD
-                  key="drop3"
-                  id="drop3"
-                  className="image__drop"
-                  drop={drop}
-                  dragOver={dragOver}
-                ></ContainerDnD>
-              </div>
-              <div className="container__drop absolute" id="item4">
-                <ContainerDnD
-                  key="drop4"
-                  id="drop4"
-                  className="image__drop"
-                  drop={drop}
-                  dragOver={dragOver}
-                ></ContainerDnD>
-              </div>
-              <div className="container__drop absolute" id="item5">
-                <ContainerDnD
-                  key="drop5"
-                  id="drop5"
-                  className="image__drop"
-                  drop={drop}
-                  dragOver={dragOver}
-                ></ContainerDnD>
-              </div>
-
-              <div
-                id={click ? (checkCircul1 ? "item6t" : "item6f") : "item6"}
-                className="container__drop absolute"
-              ></div>
-              <div
-                id={click ? (checkCircul2 ? "item7t" : "item7f") : "item7"}
-                className="container__drop absolute"
-              ></div>
-              <div
-                id={click ? (checkCircul3 ? "item8t" : "item8f") : "item8"}
-                className="container__drop absolute"
-              ></div>
-              <div
-                id={click ? (checkCircul4 ? "item9t" : "item9f") : "item9"}
-                className="container__drop absolute"
-              ></div>
-              <div
-                id={click ? (checkCircul5 ? "item10t" : "item10f") : "item10"}
-                className="container__drop absolute"
-              ></div>
-              <div className="container__drop absolute" id="item11">
-                <p className="puntuacion-titulo">Puntuación :</p>
-                <div className="puntuacion-titulo">{puntuacion}</div>
-              </div>
-              <div className="container__drop absolute" id="item12">
-                <div className="puntuacion-titulo">{reloj}</div>
-              </div>
-              {
-                <div className="container__drop absolute" id="item13">
-                  <p className="puntuacion-titulo">Intentos:</p>
-                  <div className="puntuacion-titulo">{intento}</div>
+                <div className="container__drop absolute" id="item1">
+                  <ContainerDnD
+                    key="drop1"
+                    id="drop1"
+                    className="image__drop"
+                    drop={drop}
+                    dragOver={dragOver}
+                  ></ContainerDnD>
                 </div>
-              }
+                <div className="container__drop absolute" id="item2">
+                  <ContainerDnD
+                    key="drop2"
+                    id="drop2"
+                    className="image__drop"
+                    drop={drop}
+                    dragOver={dragOver}
+                  ></ContainerDnD>
+                </div>
+                <div className="container__drop absolute" id="item3">
+                  <ContainerDnD
+                    key="drop3"
+                    id="drop3"
+                    className="image__drop"
+                    drop={drop}
+                    dragOver={dragOver}
+                  ></ContainerDnD>
+                </div>
+                <div className="container__drop absolute" id="item4">
+                  <ContainerDnD
+                    key="drop4"
+                    id="drop4"
+                    className="image__drop"
+                    drop={drop}
+                    dragOver={dragOver}
+                  ></ContainerDnD>
+                </div>
+                <div className="container__drop absolute" id="item5">
+                  <ContainerDnD
+                    key="drop5"
+                    id="drop5"
+                    className="image__drop"
+                    drop={drop}
+                    dragOver={dragOver}
+                  ></ContainerDnD>
+                </div>
+
+                <div
+                  id={click ? (checkCircul1 ? "item6t" : "item6f") : "item6"}
+                  className="container__drop absolute"
+                ></div>
+                <div
+                  id={click ? (checkCircul2 ? "item7t" : "item7f") : "item7"}
+                  className="container__drop absolute"
+                ></div>
+                <div
+                  id={click ? (checkCircul3 ? "item8t" : "item8f") : "item8"}
+                  className="container__drop absolute"
+                ></div>
+                <div
+                  id={click ? (checkCircul4 ? "item9t" : "item9f") : "item9"}
+                  className="container__drop absolute"
+                ></div>
+                <div
+                  id={click ? (checkCircul5 ? "item10t" : "item10f") : "item10"}
+                  className="container__drop absolute"
+                ></div>
+                <div className="container__drop absolute" id="item11">
+                  <p className="puntuacion-titulo">Puntuación :</p>
+                  <div className="puntuacion-titulo">{puntuacion}</div>
+                </div>
+                <div className="container__drop absolute" id="item12">
+                  <div className="puntuacion-titulo">{reloj}</div>
+                </div>
+                {
+                  <div className="container__drop absolute" id="item13">
+                    <p className="puntuacion-titulo">Intentos:</p>
+                    <div className="puntuacion-titulo">{intento}</div>
+                  </div>
+                }
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="Container-button">
-        {active ? (
-          <button
-            onClick={validacion}
-            className="css-button css-button-gradient css-button-gradient--7"
-          >
-            {" "}
-            Verificar
-          </button>
-        ) : puntuacion !== 20 ? (
-          <button
-            onClick={borrarEstilos}
-            className="css-button2 css-button-gradient css-button-gradient--7"
-          >
-            {" "}
-            Intentar Nuevamente
-          </button>
-        ) : (
-          <button
-            onClick={validacion}
-            className="css-button css-button-gradient css-button-gradient--7"
-          >
-            {" "}
-            Verificar
-          </button>
-        )}
-      </div>
+        <div className="Container-button">
+          {active ? (
+            <button
+              onClick={validacion}
+              className="css-button css-button-gradient css-button-gradient--7"
+            >
+              {" "}
+              Verificar
+            </button>
+          ) : puntuacion !== 20 ? (
+            <button
+              onClick={borrarEstilos}
+              className="css-button2 css-button-gradient css-button-gradient--7"
+            >
+              {" "}
+              Intentar Nuevamente
+            </button>
+          ) : (
+            <button
+              onClick={validacion}
+              className="css-button css-button-gradient css-button-gradient--7"
+            >
+              {" "}
+              Verificar
+            </button>
+          )}
+        </div>
 
-      {/* <FooterButtons setShowAnswer={setCorrectAnswer} />*/}
+        {/* <FooterButtons setShowAnswer={setCorrectAnswer} />*/}
+      </div>
     </div>
   );
 }
